22b521fbb102ab1715ab5be3f332561ade6534f3
andre <andre.ofuji@gmail.com>
2020-09-09 00:52:18 -0300

    Criado Configuração do projeto

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'react/jsx-filename-extension': 0,
+    'react/state-in-constructor': 0,
+    'no-console': 'off',
+    'import/no-duplicates': 'off',
+    'react/forbid-prop-types': 'off',
+  },
+};
