d39306496ddf8ddef53cfc9965005c67e98d424b
andre <andre.ofuji@gmail.com>
2020-09-10 19:24:33 -0300

    Removido console

--- src/components/starred.js
+++ src/components/starred.js
@@ -3,7 +3,7 @@ import React from 'react'
 import ListGroup from 'react-bootstrap/ListGroup'
 
 export default function Starred({ lists }) {
-  // console.log(lista)
+
   return (
     <>
       {lists.map((e) => (
